09b8087552eb68fcd61b6f05d82aa67f0971ab13
Whiteroad95 <zhanabayevakzhol@gmail.com>
2022-12-20 19:18:13 +0600

    added aps subjects and  employers

--- employers/admin.py
+++ employers/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Headmaster, Teachers, Headteacher
+from django.utils.safestring import mark_safe
 
 
 class HeadmasterAdmin(admin.ModelAdmin):
-    list_display = ['name_surname', 'photo', 'number']
+    list_display = ['name_surname', 'get_image', 'number']
+    search_fields = ['name_surname', 'number']
+    list_display_links = ['name_surname']
+    list_editable = ['number']
+    list_filter = ['number']
+
+    def get_image(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' weight='150' height='200'>")
+        return 'Images not found!'
 
 
 admin.site.register(Headmaster, HeadmasterAdmin)
